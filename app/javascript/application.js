6b2dd2545ddc74d3713e08a0faa945e99d9bd56c
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-16 20:04:47 +0300

    Setup route for the greeting component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from '../components/App';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
